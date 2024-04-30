d1a73591f134bdd51ef71514c4bd4e7d13dd8cb7
Kushal Jasani <kushal-jasani@users.noreply.github.com>
2024-04-30 12:56:31 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const bodyparser = require("body-parser");
+require("dotenv").config();
+
+app.use(bodyparser.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    next();
+});
+
+app.listen(process.env.PORT);
