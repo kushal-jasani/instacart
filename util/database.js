615522336742ef1001b7ce317d88cfb71260e418
Kushal Jasani <kushal-jasani@users.noreply.github.com>
2024-05-08 12:35:34 +0530

    db extra added

--- util/database.js
+++ util/database.js
@@ -5,7 +5,11 @@ const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  database:process.env.DB_NAME,
-password:process.env.DB_PASSWORD});
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  queueLimit: 200,
+  connectTimeout: 5000,
+  connectionLimit: 10,
+});
 
-module.exports=pool.promise();
+module.exports = pool.promise();
