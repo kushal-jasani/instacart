1fa0cf19c05ca21832605111c55c51ad3533a032
Kushal Jasani <kushal-jasani@users.noreply.github.com>
2024-05-08 16:08:34 +0530

    modified db config

--- util/database.js
+++ util/database.js
@@ -7,9 +7,6 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  queueLimit: 200,
-  connectTimeout: 5000,
-  connectionLimit: 10,
 });
 
 module.exports = pool.promise();
