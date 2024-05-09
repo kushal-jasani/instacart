54dc1eab6afae070ca1e41b68d26b1fbed5691ec
Kushal Jasani <kushal-jasani@users.noreply.github.com>
2024-05-09 12:16:57 +0530

    completed google oauth,send tokens to local storage

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express=require('express');
 const app=express();
-const session=require('express-session')
 const bodyparser = require("body-parser");
 const passport=require('passport')
 require("dotenv").config();
+const {useGoogleStrategy}=require('./util/passport');
 
-
+useGoogleStrategy();
 const authRoutes=require('./routes/auth')
 
 app.use(bodyparser.json());
-app.use(session({
-    secret: process.env.SESSION_SECRET, 
-    resave: false,
-    saveUninitialized: false
-  }));
+
 app.use(passport.initialize());
-app.use(passport.session());
+
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
@@ -23,10 +19,6 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.post('/protected-route', passport.authenticate('jwt', { session: false }), (req, res) => {
-//     // This route is protected by JWT authentication
-//     res.json({ message: 'Protected route accessed successfully!' });
-//   });
 app.use(authRoutes);
 
 app.listen(process.env.PORT);
